ce03d2b7ad88415ce5b9b9b292eb43072ff41f35
Alexeyalexeyalex <34206586+Alexeyalexeyalex@users.noreply.github.com>
2022-11-10 18:28:32 +0300

    adit cycle

--- Program.cs
+++ Program.cs
@@ -3,14 +3,20 @@
 Clear();
 WriteLine("Введите массив строк через пробел: ");
 string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-string[] resultArray = new string[array.Length];
-
+int j = 0;
 for (int i = 0; i < array.Length; i++)
 {
-    if (array[i].Length<=3)
+    if (array[i].Length <= 3) j++;
+}
+string[] resultArray = new string[j];
+j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
     {
-        resultArray[i] = array[i];
+        resultArray[j] = array[i];
+        j++;
     }
 }
 
-WriteLine($"{string.Join(" ", array)} -> {string.Join(" ", resultArray)}");
+WriteLine($"[{string.Join(",", array)}] -> [{string.Join(",", resultArray)}]");

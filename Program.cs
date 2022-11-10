913eac727a7acd4bc1deaebf343077158c80d4b9
Alexeyalexeyalex <34206586+Alexeyalexeyalex@users.noreply.github.com>
2022-11-10 18:22:35 +0300

    add result

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Clear();
+WriteLine("Введите массив строк через пробел: ");
+string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+string[] resultArray = new string[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length<=3)
+    {
+        resultArray[i] = array[i];
+    }
+}
+
+WriteLine($"{string.Join(" ", array)} -> {string.Join(" ", resultArray)}");
